d42067128464f21fa27c9919eb07363fefcc0192
liona <theajaslan@gmail.com>
2023-04-12 07:56:20 +0600

    add scralling function to list

--- app/src/main/java/com/example/first_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/first_compose_app/MainActivity.kt
@@ -1,52 +1,68 @@
 package com.example.first_compose_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
+                ListItem(name = "Aslan", prof = "Developer")
                 ListItem(name = "Aslan", prof = "Developer")
                 ListItem(name = "Aslan", prof = "Developer")
                 ListItem(name = "Aslan", prof = "Developer")
                 ListItem(name = "Aslan", prof = "Developer")
                 ListItem(name = "Aslan", prof = "Developer")
             }
-
         }
     }
 }
 
-@Preview
 @Composable
 fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
+            .padding(10.dp)
             .fillMaxWidth()
-            .padding(10.dp),
+            .pointerInput(Unit) {
+                detectTapGestures {
+
+                    Log.e("ololo", "Long press: $it")
+                }
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
@@ -70,7 +86,6 @@ fun ListItem(name: String, prof: String) {
             }
         }
     }
-
 }
 
 
